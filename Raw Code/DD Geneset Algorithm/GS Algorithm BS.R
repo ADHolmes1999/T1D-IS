5fa9b164c603cc7176719f31c95ba29c32595cd5
ADHolmes1999 <antonioholmes0@gmail.com>
2024-06-27 14:12:23 +0000

    creating a gene interaction graph

--- Raw Code/DD Geneset Algorithm/GS Algorithm BS.R
+++ Raw Code/DD Geneset Algorithm/GS Algorithm BS.R
@@ -1,18 +1,53 @@
 ### Data Driven GS Algorithm (Bootstrapping)
 ## Antonio Holmes
-# 06/07/2024
+# 06/27/2024
 
 
 # DECODING THE FUNX ------------------------------------------------------------------
 
+## What does the function require to produce the results I am looking for 
 
-# RENAME ------------------------------------------------------------------
+# Nat Log, Log FC. P-Val (adj P-val)
+# Pre-defined gene-set (w/ gene dictionary and annot)
+# modeling gene expression
+# pseudolilikehood funx
+
+# Install & Load Packages ------------------------------------------------------------------
+
+install.packages(c("igraph", "ggplot2"))
+BiocManager::install("clusterProfiler")
+BiocManager::install("ReactomePA",force = TRUE)
+
+library(igraph)
+library(clusterProfiler)
+library(ReactomePA)
+library(ggplot2)
 
 
-# RENAME ------------------------------------------------------------------
+# Gene Set & Pathways ------------------------------------------------------------------
 
+#Example gene list
+genes <- c("11171", "8243", "112464", "2194",
+           "9318", "79026", "1654", "65003",
+           "6240", "3476", "6238", "3836",
+           "4176", "1017", "249")
 
-# RENAME ------------------------------------------------------------------
+# Pathway enrichment analysis
+enriched_pathways <- enrichPathway(gene = genes, organism = "human")
 
+# Extract pathway and gene information
+pathways <- as.data.frame(enriched_pathways)
 
-# RENAME ------------------------------------------------------------------
+# CAN I REUSE THE CODE BELOW PIPED INTO THE PATHFINDR FUNX
+
+`# Create an edge list for the graph
+edge_list <- data.frame(
+  from = rep(pathways$Description, each = length(genes)),
+  to = rep(genes, times = nrow(pathways)))
+
+# Graph & Results ------------------------------------------------------------------
+
+# Create the graph
+g <- graph_from_data_frame(edge_list, directed = FALSE)
+
+plot(g, vertex.label.cex = 0.8, vertex.size = 5)
